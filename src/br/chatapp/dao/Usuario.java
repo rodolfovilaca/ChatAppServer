2af668ae9fff2093ff57d34a97aa0dd3b73fc9ca
rodolfovilaca <rodolfovilaca91@gmail.com>
2017-06-17 01:31:55 -0300

    .jar concertados

--- src/br/chatapp/dao/Usuario.java
+++ src/br/chatapp/dao/Usuario.java
@@ -4,14 +4,11 @@ import java.io.Serializable;
 
 import org.rodolfo.bancodedadosmysql.BancoDeDados;
 
-
 public class Usuario implements Serializable{
 	static final long serialVersionUID = 3632977338254009699L;
 
     private String nome;
     private final String ADICIONAR_USUARIO = "INSERT INTO Usuario (usuario_nome) VALUES";
-    private final String BUSCAR_USUARIO = "SELECT usuario_id FROM Usuario WHERE usuario_nome LIKE '%"+this.getNome()+"%';";
-    private final String BUSCAR_USUARIO_INICIO = "SELECT COUNT(*) FROM Usuario WHERE usuario_nome LIKE '%"+this.getNome()+"%';";
 
     public Usuario(String nome) {
         this.nome = nome;
@@ -30,12 +27,12 @@ public class Usuario implements Serializable{
     }
     
     public int getForeignKeyId(){
-    	int keyId = BancoDeDados.buscarId(BUSCAR_USUARIO);
+    	int keyId = BancoDeDados.buscarId("SELECT usuario_id FROM Usuario WHERE usuario_nome = '"+this.getNome()+"';");
     	return keyId;
     }
     
     public boolean salvarBancoDeDados() {
-    	int buscaUsuario = BancoDeDados.buscarId(BUSCAR_USUARIO_INICIO);
+    	int buscaUsuario = BancoDeDados.buscarId("SELECT * FROM Usuario WHERE usuario_nome = '"+this.getNome()+"';");
     	if(buscaUsuario == 0){
     		BancoDeDados.inserir(ADICIONAR_USUARIO+"('"+this.getNome()+"')");
     	}
